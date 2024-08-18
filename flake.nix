3c630acb3c99774b28de227f2d7cc75193adf7b3
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-18 21:07:05 +0200

    added home-manager-import to nixos/configuration.nix

--- flake.nix
+++ flake.nix
@@ -3,27 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    # you can also add the nixos stable channel here if u dont want unstable
+    # you can add the stable channel here if u dont want unstable
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs }@inputs: 
   let
     system = "x86_64-linux";
-
     pkgs = import nixpkgs {
       inherit system;
-
       config = {
         allowUnfree = true;
       };
     };
-
   in 
   {
 
     nixosConfigurations = {
     nixos-config = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit system; };
+      specialArgs = { inherit inputs system; };
 
       modules = [
       ./nixos/configuration.nix
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,13 +2,21 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ inputs, config, pkgs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.home-manager
     ];
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      jonash = import ./home.nix;
+    };
+  };
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
