558abf26e221cb2b4b434db3bead583fe8af99f2
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-20 00:18:02 +0200

    changing to setting env variables instead

--- modules/variables.nix
+++ modules/variables.nix
@@ -8,6 +8,11 @@
     VISUAL = "nvim";
     TERMINAL = "kitty";
     BROWSER = "qutebrowser";
+    XDG_DEFAULT_PDF_VIEWER = "zathura";
+    XDG_DEFAULT_IMAGE_VIEWER = "nsxiv";
+    XDG_DEFAULT_VIDEO_PLAYER = "mpv";
+    XDG_DEFAULT_BROWSER = "qutebrowser";
+
   };
 
 
@@ -23,21 +28,15 @@
   xdg.mime.enable = true;
 
   # Set up XDG MIME types
-  xdg.mimeAppsList = {
-    "video/mp4" = [ "mpv.desktop" ];
-    "video/x-matroska" = [ "mpv.desktop" ];
-    "image/jpeg" = [ "nsxiv.desktop" ];
-    "image/png" = [ "nsxiv.desktop" ];
-    "application/pdf" = [ "zathura.desktop" ];
-    "text/html" = [ "qutebrowser.desktop" ];
-  };
-
-  # Optionally, set environment variables for default apps
-  # environment.variables = {
-  #   XDG_DEFAULT_PDF_VIEWER = "zathura";
-  #   XDG_DEFAULT_IMAGE_VIEWER = "nsxiv";
-  #   XDG_DEFAULT_VIDEO_PLAYER = "mpv";
-  #   XDG_DEFAULT_BROWSER = "qutebrowser";
+  # xdg.mimeAppsList = {
+  #   "video/mp4" = [ "mpv.desktop" ];
+  #   "video/x-matroska" = [ "mpv.desktop" ];
+  #   "image/jpeg" = [ "nsxiv.desktop" ];
+  #   "image/png" = [ "nsxiv.desktop" ];
+  #   "application/pdf" = [ "zathura.desktop" ];
+  #   "text/html" = [ "qutebrowser.desktop" ];
   # };
+
+   
 
 }
