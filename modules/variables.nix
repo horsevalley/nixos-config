c750907a4b11865a10df8e14f31349c09143edb9
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-19 23:50:20 +0200

    added xdg mime types to variables.nix

--- modules/variables.nix
+++ modules/variables.nix
@@ -9,7 +9,6 @@
     BROWSER = "qutebrowser";
   };
 
-  xdg.mime.enable = true;
 
   # Set the custom path for .desktop files
   environment.sessionVariables = {
@@ -17,6 +16,19 @@
       "/home/jonash/.local/share"
       "$XDG_DATA_DIRS"
     ];
+  };
+
+  # Enable xdg mime
+  xdg.mime.enable = true;
+
+  # Set up XDG MIME types
+  xdg.mimeTypes = {
+    "video/mp4" = "mpv.desktop";
+    "video/x-matroska" = "mpv.desktop";
+    "image/jpeg" = "nsxiv.desktop";
+    "image/png" = "nsxiv.desktop";
+    "application/pdf" = "zathura.desktop";
+    "text/html" = "qutebrowser.desktop";
   };
 
 }
