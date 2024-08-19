4ce84a61b98b6179432e65080cf143c7d187a25d
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-20 00:16:29 +0200

    renamed mimeApps to mimeAppsList

--- modules/variables.nix
+++ modules/variables.nix
@@ -23,7 +23,7 @@
   xdg.mime.enable = true;
 
   # Set up XDG MIME types
-  xdg.mimeApps = {
+  xdg.mimeAppsList = {
     "video/mp4" = [ "mpv.desktop" ];
     "video/x-matroska" = [ "mpv.desktop" ];
     "image/jpeg" = [ "nsxiv.desktop" ];
@@ -31,5 +31,13 @@
     "application/pdf" = [ "zathura.desktop" ];
     "text/html" = [ "qutebrowser.desktop" ];
   };
+
+  # Optionally, set environment variables for default apps
+  # environment.variables = {
+  #   XDG_DEFAULT_PDF_VIEWER = "zathura";
+  #   XDG_DEFAULT_IMAGE_VIEWER = "nsxiv";
+  #   XDG_DEFAULT_VIDEO_PLAYER = "mpv";
+  #   XDG_DEFAULT_BROWSER = "qutebrowser";
+  # };
 
 }
