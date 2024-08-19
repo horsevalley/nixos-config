e77dfe9485cba7e577507895b233b1d62f2a5fd6
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-20 00:19:10 +0200

    using env variables instead of xdg mime types

--- modules/variables.nix
+++ modules/variables.nix
@@ -27,16 +27,4 @@
   # Enable xdg mime
   xdg.mime.enable = true;
 
-  # Set up XDG MIME types
-  # xdg.mimeAppsList = {
-  #   "video/mp4" = [ "mpv.desktop" ];
-  #   "video/x-matroska" = [ "mpv.desktop" ];
-  #   "image/jpeg" = [ "nsxiv.desktop" ];
-  #   "image/png" = [ "nsxiv.desktop" ];
-  #   "application/pdf" = [ "zathura.desktop" ];
-  #   "text/html" = [ "qutebrowser.desktop" ];
-  # };
-
-   
-
 }
