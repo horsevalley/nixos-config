7cfb35d5e841f627e60229ae7241a2dbd397751e
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-21 11:55:51 +0200

    moved env variables from hyprland.nix to hypr/hyprland.conf

--- modules/programs/wayland/hyprland.nix
+++ modules/programs/wayland/hyprland.nix
@@ -53,11 +53,11 @@
   };
 
   # Environment variables for NVIDIA and Wayland
-  environment.variables = {
-    LIBVA_DRIVER_NAME = "nvidia";
-    XDG_SESSION_TYPE = "wayland";
-    GBM_BACKEND = "nvidia-drm";
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    WLR_NO_HARDWARE_CURSORS = "1";
-  };
+  # environment.variables = {
+  #   LIBVA_DRIVER_NAME = "nvidia";
+  #   XDG_SESSION_TYPE = "wayland";
+  #   GBM_BACKEND = "nvidia-drm";
+  #   __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+  #   WLR_NO_HARDWARE_CURSORS = "1";
+  # };
 }
