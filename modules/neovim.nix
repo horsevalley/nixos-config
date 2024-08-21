d7df3f8125d911af8bd4db79df093eec8bb0b3f7
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-21 17:33:43 +0200

    fixing typo in neovim.nix

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,18 +1,22 @@
-let
+{ config, pkgs, ... }:
+
+
+  let
   unstablePkgs = import (fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
     sha256 = "0awagdjzv2fsy5v7a0wxz1hd642gsglib2gk4lnqm0iybly7kf0s";  # You need find or generate this hash yourself
   }) 
   
-in {
+  in {
   environment.systemPackages = with unstablePkgs; [
     neovim
   ];
 
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    vimAlias = true;
-    viAlias = true;
-  };
-}
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true;
+      vimAlias = true;
+      viAlias = true;
+    };
+  }
+
