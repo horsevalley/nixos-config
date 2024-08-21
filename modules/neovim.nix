633fbf496d680d1a9aed91985b0990efdfdbeed8
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-21 17:29:53 +0200

    fixing typo in neovim.nix

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -2,9 +2,8 @@ let
   unstablePkgs = import (fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
     sha256 = "0awagdjzv2fsy5v7a0wxz1hd642gsglib2gk4lnqm0iybly7kf0s";  # You need find or generate this hash yourself
-  }) {
-    system = pkgs.system;
-  };
+  }) 
+  
 in {
   environment.systemPackages = with unstablePkgs; [
     neovim
