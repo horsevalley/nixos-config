e77a17b9a2c1ae3145dc157f093f2a2d8a816ab9
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-21 17:25:18 +0200

    added neovim.nix which is unstable channel

---
+++ modules/neovim.nix
@@ -0,0 +1,19 @@
+let
+  unstablegs = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+    sha256 = "0awagdjzv2fsy5v7a0wxz1hd642gsglib2gk4lnqm0iybly7kf0s";  # You need find or generate this hash yourself
+  }) {
+    system = pkgs.system;
+  };
+in {
+  environment.systemPackages = with unstablePkgs; [
+    neovim
+  ];
+
+  programs.neovim = {
+    = true;
+    defaultEditor = true;
+    vimAlias = true;
+    viAlias = true;
+  };
+}
