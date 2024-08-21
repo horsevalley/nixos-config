fe33e2ddba063f6e933d31ba6158f2d1d5fa135d
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-21 17:40:09 +0200

    fixing typos in neovim.nix

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,22 +1,27 @@
 { config, pkgs, ... }:
 
-
-  let
-  unstablePkgs = import (fetchTarball {
+let
+  unstable = import (fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
-    sha256 = "0awagdjzv2fsy5v7a0wxz1hd642gsglib2gk4lnqm0iybly7kf0s";  # You need find or generate this hash yourself
-  }) 
-  
-  in {
-  environment.systemPackages = with unstablePkgs; [
+    sha256 = "0awagdjzv2fsy5v7a0wxz1hd642gsglib2gk4lnqm0iybly7kf0s";
+  }) { 
+    # Assuming you're on the same system architecture as your NixOS configuration
+    system = pkgs.system;
+  };
+in {
+  # Use Neovim from the unstable channel
+  environment.systemPackages = with unstable; [
     neovim
   ];
 
-    programs.neovim = {
-      enable = true;
-      defaultEditor = true;
-      vimAlias = true;
-      viAlias = true;
-    };
-  }
+  # Optionally, configure Neovim to be the default editor
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    vimAlias = true;
+    viAlias = true;
+  };
 
+  # You might want to add more Neovim specific configurations here, like plugins or custom settings
+  # For example, if you use a plugin manager like vim-plug or packer.nvim, you would configure it here or in your Neovim init file.
+}
