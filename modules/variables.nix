9211a8ed21c350314cf8b8c8159dda1ea04fae9f
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-22 19:10:58 +0200

    removing a couple of session variables from variables.nix

--- modules/variables.nix
+++ modules/variables.nix
@@ -22,18 +22,6 @@
       "/home/jonash/.local/share"
       "$XDG_DATA_DIRS"
     ];
-    XDG_CONFIG_HOME = [ 
-      "/home/jonash/.config"
-      "$XDG_CONFIG_HOME"
-    ];
-    XDG_DATA_HOME = [ 
-      "/home/jonash/.local/share"
-      "$XDG_DATA_HOME"
-    ];
-    XDG_CACHE_HOME = [ 
-      "/home/jonash/.cache"
-      "$XDG_CACHE_HOME"
-    ];
   };
 
   # Enable xdg mime
