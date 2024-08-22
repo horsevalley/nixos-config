95547a608184827eb16b063e14b241b064e64609
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-22 16:44:45 +0200

    fixing typos in pcmanfm.nix

--- modules/pcmanfm.nix
+++ modules/pcmanfm.nix
@@ -1,16 +1,14 @@
 { config, pkgs, ... }:
 
-{
-
-  # let
-  #   theme = pkgs.gtk3.kde-gtk-config;  # Or choose another GTK theme package
-  #   icons = pkgs.adwaita-icon-theme;    # Or choose another icon theme package
-  # in {
-  #   environment.systemPackages = with pkgs; [
-  #     pcmanfm
-  #     theme
-  #     icons
-  #   ];
+let
+  theme = pkgs.gtk3.kde-gtk-config;  # Or choose another GTK theme package
+  icons = pkgs.adwaita-icon-theme;    # Or choose another icon theme package
+in {
+  environment.systemPackages = with pkgs; [
+    pcmanfm
+    theme
+    icons
+  ];
 
   # GTK settings for pcmanfm
   environment.variables = {
@@ -19,7 +17,6 @@
   };
 
   # Example of applying transparency (This may depend on the window manager/compositor configuration)
-  services.gtk3.enable = true;
   environment.etc."xdg/gtk-3.0/settings.ini".text = ''
     [Settings]
     gtk-application-prefer-dark-theme=1
@@ -28,10 +25,28 @@
     gtk-enable-animations=true
   '';
   
-  # Configure pcmanfm to use the dark theme and icons
-  programs.pcmanfm = {
-    enable = true;
-    configFile = ~/.config/pcmanfm/default/pcmanfm.conf;
+  # Hyprland-specific settings for transparency (this assumes you have Hyprland configured for transparency)
+  programs.hyprland.settings = {
+    decoration {
+      blur = true;  # if you want blurred transparency
+    }
+    # Set other Hyprland transparency settings as needed
   };
 
-}
+  # Configure pcmanfm settings declaratively
+  environment.etc."xdg/pcmanfm/default/pcmanfm.conf".text = ''
+    [Desktop]
+    wallpaper_mode=stretch
+    wallpaper=/path/to/your/wallpaper
+
+    [GTK]
+    theme=Adwaita-dark
+    icons=Adwaita
+    enable_transparency=true
+
+    [UI]
+    always_show_tabs=true
+  '';
+
+};
+
