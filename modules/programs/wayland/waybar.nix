75a657d2ad64278e7a9846675b1a8ceb69b72431
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-23 14:44:19 +0200

    removing config from waybar.nix

--- modules/programs/wayland/waybar.nix
+++ modules/programs/wayland/waybar.nix
@@ -5,54 +5,6 @@
   programs.waybar = {
   enable = true;
   systemd.enable = true;
-    config = {
-      layer = "bottom";
-      height = 30;
-      modulesRight = [
-        "network"
-        "battery"
-        "clock"
-      ];
-      modulesLeft = [
-        "workspaces"
-      ];
-
-      workspaces = {
-        format = "{name}";
-        maxLength = 15;
-        spacing = 5;
-        padding = 10;
-        hideIfEmpty = true;
-        icons = true;
-      };
-
-      network = {
-        format = "{ifname}: {rate} ↓ {rate_up} ↑";
-        formatConnected = "{ifname}: {rate} ↓ {rate_up} ↑";
-        formatDisconnected = "No Network";
-        tooltip = true;
-      };
-
-      battery = {
-        format = "{percentage}% {icon}";
-        formatCharging = "{percentage}% {icon} (Charging)";
-        formatDischarging = "{percentage}% {icon}";
-        formatFull = "Fully Charged";
-        tooltip = true;
-      };
-
-      clock = {
-        format = "%Y-%m-%d %H:%M:%S";
-        tooltip = false;
-      };
-
-      appearance = {
-        background = "#282c34";
-        foreground = "#ffffff";
-        border = "#4c566a";
-        font = "monospace 10";
-      };
-    };
   };
 
 }
