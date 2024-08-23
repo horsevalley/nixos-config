75266fdab43ba61503ec509d51db7d21b2e8a3ca
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-23 14:39:24 +0200

    fixing waybar.nix

--- modules/programs/wayland/waybar.nix
+++ modules/programs/wayland/waybar.nix
@@ -5,53 +5,54 @@
   programs.waybar = {
   enable = true;
   systemd.enable = true;
-  settings = [{
+    config = {
+      layer = "bottom";
+      height = 30;
+      modulesRight = [
+        "network"
+        "battery"
+        "clock"
+      ];
+      modulesLeft = [
+        "workspaces"
+      ];
 
-  "layer": "top",
-  "height": 30,
-  "modules-right": [
-    "network",
-    "battery",
-    "clock"
-  ],
-  "modules-left": [
-    "workspaces"
-  ],
-  "workspaces": {
-    "format": "{name}",
-    "max-length": 15,
-    "spacing": 5,
-    "padding": 10,
-    "tooltip": false,
-    "hide-if-empty": true,
-    "icons": true
-  },
-  "network": {
-    "format": "{ifname}: {rate} ↓ {rate_up} ↑",
-    "format-connected": "{ifname}: {rate} ↓ {rate_up} ↑",
-    "format-disconnected": "No Network",
-    "tooltip": true
-  },
-  "battery": {
-    "format": "{percentage}% {icon}",
-    "format-charging": "{percentage}% {icon} (Charging)",
-    "format-discharging": "{percentage}% {icon}",
-    "format-full": "Fully Charged",
-    "tooltip": true
-  },
-  "clock": {
-    "format": "%Y-%m-%d %H:%M:%S",
-    "tooltip": false
-  },
-  "appearance": {
-    "background": "#282c34",
-    "foreground": "#ffffff",
-    "border": "#4c566a",
-    "font": "monospace 10"
-  }
+      workspaces = {
+        format = "{name}";
+        maxLength = 15;
+        spacing = 5;
+        padding = 10;
+        hideIfEmpty = true;
+        icons = true;
+      };
 
+      network = {
+        format = "{ifname}: {rate} ↓ {rate_up} ↑";
+        formatConnected = "{ifname}: {rate} ↓ {rate_up} ↑";
+        formatDisconnected = "No Network";
+        tooltip = true;
+      };
 
-  }];
-};
+      battery = {
+        format = "{percentage}% {icon}";
+        formatCharging = "{percentage}% {icon} (Charging)";
+        formatDischarging = "{percentage}% {icon}";
+        formatFull = "Fully Charged";
+        tooltip = true;
+      };
+
+      clock = {
+        format = "%Y-%m-%d %H:%M:%S";
+        tooltip = false;
+      };
+
+      appearance = {
+        background = "#282c34";
+        foreground = "#ffffff";
+        border = "#4c566a";
+        font = "monospace 10";
+      };
+    };
+  };
 
 }
