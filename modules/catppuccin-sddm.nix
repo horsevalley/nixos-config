ef3dd5cdfdf0e3f270043e7344d276f864a04a1e
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-23 19:06:11 +0200

    fixing typos in catppuccin-sddm.nix

--- modules/catppuccin-sddm.nix
+++ modules/catppuccin-sddm.nix
@@ -1,28 +1,28 @@
 { config, lib, pkgs, ... }:
 
-{
-
-  pkgs.stdenv.mkDerivation = {
+let
+  catppuccin-sddm = pkgs.stdenv.mkDerivation {
     pname = "catppuccin-sddm";
     version = "0.1.0";
 
-  src = pkgs.fetchFromGitHub {
-    owner = "catppuccin";
-    repo = "sddm";
-    rev = "7fc67d1027cdb7f4d833c5d23a8c34a0029b0661";
-    sha256 = "sha256-SjYwyUvvx/ageqVH5MmYmHNRKNvvnF3DYMJ/f2/L+Go=";
-  };
+    src = pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "sddm";
+      rev = "7fc67d1027cdb7f4d833c5d23a8c34a0029b0661";
+      sha256 = "sha256-SjYwyUvvx/ageqVH5MmYmHNRKNvvnF3DYMJ/f2/L+Go=";
+    };
 
-  installPhase = ''
-    mkdir -p $out/share/sddm/themes
-    cp -r src/catppuccin-mocha $out/share/sddm/themes/catppuccin
-  '';
-
-  meta = with lib; {
-    description = "Catppuccin theme for SDDM";
-    homepage = "https://github.com/catppuccin/sddm";
-    license = licenses.mit;
-    platforms = platforms.all;
+    installPhase = ''
+      mkdir -p $out/share/sddm/themes
+      cp -r src/catppuccin-mocha $out/share/sddm/themes/catppuccin
+    '';
   };
+in
+{
+  environment.systemPackages = [ catppuccin-sddm ];
+
+  services.xserver.displayManager.sddm = {
+    enable = true;
+    theme = "catppuccin";
   };
 }
