292acee2b158e27dbb79759be6a3558d595ee0ba
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-23 19:10:36 +0200

    fixed sha256 hash

--- modules/packages.nix
+++ modules/packages.nix
@@ -179,7 +179,9 @@
       owner = "catppuccin";
       repo = "sddm";
       rev = "7fc67d1027cdb7f4d833c5d23a8c34a0029b0661"; # Check for the latest commit hash
-      sha256 = "fOVgWNE8NWbWHlu+EAmxSz+QJ6y+IODc/u3UF+JWQqA=";
+      sha256 = "0spqrdppyzy2c31mv77gvcl52wwqk34y8ix5gahgdizg9g4k0dja";
+
+
     })
 
   ];
