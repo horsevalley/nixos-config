61812d182fd3a4eb1bd6a75047c9d5dd370887d7
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-23 14:55:20 +0200

    fixing waybar

--- modules/programs/wayland/waybar.nix
+++ modules/programs/wayland/waybar.nix
@@ -6,4 +6,62 @@
   enable = true;
   };
 
+  { config, pkgs, ... }:
+
+
+  # Enable Waybar
+  services.waybar = {
+    enable = true;
+    # Point to the Waybar config file
+    package = pkgs.waybar;
+    extraConfig = ''
+      # The contents of your Waybar config file
+      {
+        "layer": "bottom",
+        "height": 30,
+        "modules-right": [
+          "network",
+          "battery",
+          "clock"
+        ],
+        "modules-left": [
+          "workspaces"
+        ],
+        "workspaces": {
+          "format": "{name}",
+          "max-length": 15,
+          "spacing": 5,
+          "padding": 10,
+          "tooltip": false,
+          "hide-if-empty": true,
+          "icons": true
+        },
+        "network": {
+          "format": "{ifname}: {rate} ↓ {rate_up} ↑",
+          "format-connected": "{ifname}: {rate} ↓ {rate_up} ↑",
+          "format-disconnected": "No Network",
+          "tooltip": true
+        },
+        "battery": {
+          "format": "{percentage}% {icon}",
+          "format-charging": "{percentage}% {icon} (Charging)",
+          "format-discharging": "{percentage}% {icon}",
+          "format-full": "Fully Charged",
+          "tooltip": true
+        },
+        "clock": {
+          "format": "%Y-%m-%d %H:%M:%S",
+          "tooltip": false
+        },
+        "appearance": {
+          "background": "#282c34",
+          "foreground": "#ffffff",
+          "border": "#4c566a",
+          "font": "monospace 10"
+        }
+      }
+    '';
+  };
+
+
 }
