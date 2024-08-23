f174e9d6070adedf100c0bd473e8bdda12388a61
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-23 18:21:55 +0200

    fixing typos in catppuccin-sddm.nix

--- modules/catppuccin-sddm.nix
+++ modules/catppuccin-sddm.nix
@@ -1,6 +1,6 @@
-{ stdenv, fetchFromGitHub }:
+{ lib, stdenv, fetchFromGitHub }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   pname = "catppuccin-sddm";
   version = "0.1.0";
 
@@ -8,11 +8,18 @@ stdenv.mkDerivation {
     owner = "catppuccin";
     repo = "sddm";
     rev = "7fc67d1027cdb7f4d833c5d23a8c34a0029b0661"; # Check for the latest commit hash
-    sha256 = "fOVgWNE8NWbWHlu+EAmxSz+QJ6y+IODc/u3UF+JWQqA=";
+    sha256 = "sha256-Wf1q/ivK7hI2ND1JnLMf0YDLGhhAtZ4bRJ6DaNrxO9M=";
   };
 
   installPhase = ''
     mkdir -p $out/share/sddm/themes
     cp -r src/catppuccin-mocha $out/share/sddm/themes/catppuccin
   '';
+
+  meta = with lib; {
+    description = "Catppuccin theme for SDDM";
+    homepage = "https://github.com/catppuccin/sddm";
+    license = licenses.mit;
+    platforms = platforms.all;
+  };
 }
