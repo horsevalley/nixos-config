d269015563e1ad278484ec90b0ebd0aa890268f4
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-23 19:09:17 +0200

    fixing sha256 hash in catppuccin-sddm.nix

--- modules/catppuccin-sddm.nix
+++ modules/catppuccin-sddm.nix
@@ -9,7 +9,7 @@ let
       owner = "catppuccin";
       repo = "sddm";
       rev = "7fc67d1027cdb7f4d833c5d23a8c34a0029b0661";
-      sha256 = "sha256-SjYwyUvvx/ageqVH5MmYmHNRKNvvnF3DYMJ/f2/L+Go=";
+      sha256 = "0spqrdppyzy2c31mv77gvcl52wwqk34y8ix5gahgdizg9g4k0dja";
     };
 
     installPhase = ''
@@ -21,7 +21,7 @@ in
 {
   environment.systemPackages = [ catppuccin-sddm ];
 
-  services.xserver.displayManager.sddm = {
+  services.displayManager.sddm = {
     enable = true;
     theme = "catppuccin";
   };
