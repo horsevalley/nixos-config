a14b439d8124666adaf310b7bde92f0f9967a265
Jonas Hestdahl <hestdahl@gmail.com>
2024-08-23 19:03:24 +0200

    fixing typos in catppuccin-sddm.nix

--- modules/catppuccin-sddm.nix
+++ modules/catppuccin-sddm.nix
@@ -6,7 +6,7 @@
     pname = "catppuccin-sddm";
     version = "0.1.0";
 
-  src = fetchFromGitHub {
+  src = pkgs.fetchFromGitHub {
     owner = "catppuccin";
     repo = "sddm";
     rev = "7fc67d1027cdb7f4d833c5d23a8c34a0029b0661";
