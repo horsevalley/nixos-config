3f65defdd8ea4bfe56c88786d5e178f451ef06dd
Jonas Hestdahl <hestdahl@gmail.com>
2024-09-04 22:43:39 +0200

    trying to get rid of manually adding sha256 hash to neovim

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,22 +1,31 @@
 { config, pkgs, ... }:
+
 let
-  unstable = import (builtins.fetchTarball {
+  unstableTarball = fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
-    # Instead of specifying the hash here, we'll let Nix handle it
-  }) { 
+    # Optional: Specify a hash and update it less frequently
+    # sha256 = "0000000000000000000000000000000000000000000000000000";
+  };
+  
+  # This allows you to override the unstable version if needed
+  unstable = import (unstableTarball) {
     system = pkgs.system;
+    config = config.nixpkgs.config;
   };
 in {
+  # Use Neovim from the unstable channel
   environment.systemPackages = with unstable; [
     neovim
   ];
 
+  # Configure Neovim
   programs.neovim = {
     enable = true;
     defaultEditor = true;
     vimAlias = true;
     viAlias = true;
+    # Add any additional Neovim configurations here
   };
 
-  # Other Neovim configurations...
+
 }
