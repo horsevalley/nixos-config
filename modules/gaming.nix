b10838d3e4df5e2a4de93355bcf242c528eb3e2c
Jonas Hestdahl <hestdahl@gmail.com>
2024-09-05 17:21:30 +0200

    removed steam options from gaming.nix

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -40,11 +40,4 @@
   # USAGE
   # gamescope -w 1920 -h 1080 -- /path/to/your/game
 
-  steam = {
-      enable = true;
-      gamescopeSession.enable = true;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
-    };
-
 }
