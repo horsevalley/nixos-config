d0f8300be706bbf4f84b704abe46ee4faf63672f
Jonas Hestdahl <hestdahl@gmail.com>
2024-09-05 11:06:37 +0200

    updated laptop modules

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -4,28 +4,28 @@
 
   imports = [
      ./hardware-configuration.nix
-     ../../modules./audio.nix
-     ../../modules./X11.nix
-     ../../modules./gnome.nix
-     ../../modules./programs/wayland/hyprland.nix
-     ../../modules./fonts.nix
-     ../../modules./hardware/nvidia.nix
-     ../../modules./hardware/opengl.nix
-     ../../modules./keyboard.nix
-     ../../modules./localization.nix
-     ../../modules./networking.nix
-     ../../modules./packages.nix
-     ../../modules./security.nix
-     ../../modules./services.nix
-     ../../modules./shell.nix
-     ../../modules./system.nix
-     ../../modules./users.nix
-     ../../modules./variables.nix
-     ../../modules./pcmanfm.nix
-     ../../modules./sddm.nix
-     ../../modules./git.nix
-     ../../modules./librewolf.nix
-     ../../modules./gaming.nix
+     ../../modules/audio.nix
+     ../../modules/X11.nix
+     ../../modules/gnome.nix
+     ../../modules/programs/wayland/hyprland.nix
+     ../../modules/fonts.nix
+     ../../modules/hardware/nvidia.nix
+     ../../modules/hardware/opengl.nix
+     ../../modules/keyboard.nix
+     ../../modules/localization.nix
+     ../../modules/networking.nix
+     ../../modules/packages.nix
+     ../../modules/security.nix
+     ../../modules/services.nix
+     ../../modules/shell.nix
+     ../../modules/system.nix
+     ../../modules/users.nix
+     ../../modules/variables.nix
+     ../../modules/pcmanfm.nix
+     ../../modules/sddm.nix
+     ../../modules/git.nix
+     ../../modules/librewolf.nix
+     ../../modules/gaming.nix
 
   ];
 
