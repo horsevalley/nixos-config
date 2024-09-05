7e4ad62909345e95e655476e6f9bd9141ad4d945
Jonas Hestdahl <hestdahl@gmail.com>
2024-09-05 17:20:39 +0200

    added steam options in gaming.nix

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -39,4 +39,12 @@
 
   # USAGE
   # gamescope -w 1920 -h 1080 -- /path/to/your/game
+
+  steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+    };
+
 }
