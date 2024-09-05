23213455777de46358d789ba2e9a2e38d2d8fb76
Jonas Hestdahl <hestdahl@gmail.com>
2024-09-05 18:32:23 +0200

    neovim still needs sha256

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,33 +1,27 @@
 { config, pkgs, ... }:
 
-{
-
-# let
-#   unstable = import (fetchTarball {
-#     url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
-#     # sha256 = "1vc8bzz04ni7l15a9yd1x7jn0bw2b6rszg1krp6bcxyj3910pwb7";
-#     # sha256 = "1dmng7f5rv4hgd0b61chqx589ra7jajsrzw21n8gp8makw5khvb2";
-#   }) { 
-#     # Assuming you're on the same system architecture as your NixOS configuration
-#     system = pkgs.system;
-#   };
-# in {
-#   # Use Neovim from the unstable channel
-#   environment.systemPackages = with unstable; [
-#     # neovim
-#     neovim-unwrapped
-#   ];
 
 
-  # Optionally, configure Neovim to be the default editor
+let
+  unstable = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+    sha256 = "1vc8bzz04ni7l15a9yd1x7jn0bw2b6rszg1krp6bcxyj3910pwb7";
+    # sha256 = "1dmng7f5rv4hgd0b61chqx589ra7jajsrzw21n8gp8makw5khvb2";
+  }) { 
+    # Assuming you're on the same system architecture as your NixOS configuration
+     system = pkgs.system;
+     };
+in {
+  # Use Neovim from the unstable channel
+  environment.systemPackages = with unstable; [
+    neovim
+  ];
+
   programs.neovim = {
     enable = true;
-    package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
     defaultEditor = true;
     vimAlias = true;
     viAlias = true;
   };
 
-  # You might want to add more Neovim specific configurations here, like plugins or custom settings
-  # For example, if you use a plugin manager like vim-plug or packer.nvim, you would configure it here or in your Neovim init file.
 }
