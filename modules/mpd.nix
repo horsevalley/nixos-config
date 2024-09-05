741e7593029bfb1bdffb1e38e15f45eccac80425
Jonas Hestdahl <hestdahl@gmail.com>
2024-09-05 23:09:44 +0200

    updated mpd.nix

--- modules/mpd.nix
+++ modules/mpd.nix
@@ -6,13 +6,12 @@
     musicDirectory = "/home/jonash/Music";  # Adjust this path
     extraConfig = ''
       audio_output {
-        type "pulse"
-        name "PulseAudio"
+        type "pipewire"
+        name "PipeWire Sound Server"
       }
       bind_to_address "127.0.0.1"
-      port "6600"
+      port "6601"  # Changed from 6600 to 6601
     '';
-    # If you want MPD to run as your user instead of the mpd user
     user = "jonash";  # Replace with your username
   };
 
@@ -22,9 +21,22 @@
     ncmpcpp
   ];
 
-  # Allow your user to access MPD
-  users.users.jonash.extraGroups = [ "audio" ];
+  # Allow your user to access MPD and audio
+  users.users.jonash.extraGroups = [ "audio" "pipewire" ];
 
-  # Open the firewall port for MPD (if needed)
-  networking.firewall.allowedTCPPorts = [ 6600 ];
+  # Enable PipeWire
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Ensure the MPD user has access to the PipeWire socket
+  systemd.services.mpd.serviceConfig = {
+    SupplementaryGroups = [ "pipewire" ];
+  };
+
+  # Open the firewall port for MPD
+  networking.firewall.allowedTCPPorts = [ 6601 ];  # Changed from 6600 to 6601
 }
