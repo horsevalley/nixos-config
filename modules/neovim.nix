7a3d08e1bc7da0a46b437c3317b1aaba15c88912
Jonas Hestdahl <hestdahl@gmail.com>
2024-09-08 00:51:25 +0200

    fixing neovim.nix

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -11,24 +11,23 @@ in
     unstablePkgs.neovim
   ];
 
-  # Additional Neovim-related configurations can go here
 }
 
-# let
-#   unstable = import (fetchTarball {
-#     url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
-#     # sha256 = "1vc8bzz04ni7l15a9yd1x7jn0bw2b6rszg1krp6bcxyj3910pwb7";
-#     sha256 = "0s6h7r9jin9sd8l85hdjwl3jsvzkddn3blggy78w4f21qa3chymz";
-#     # sha256 = "1dmng7f5rv4hgd0b61chqx589ra7jajsrzw21n8gp8makw5khvb2";
-#   }) { 
-#     # Assuming you're on the same system architecture as your NixOS configuration
-#      system = pkgs.system;
-#      };
-# in {
-#   # Use Neovim from the unstable channel
-#   environment.systemPackages = with unstable; [
-#     neovim
-#   ];
+ # let
+ #   unstable = import (fetchTarball {
+ #     url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+ #     # sha256 = "1vc8bzz04ni7l15a9yd1x7jn0bw2b6rszg1krp6bcxyj3910pwb7";
+ #     sha256 = "0s6h7r9jin9sd8l85hdjwl3jsvzkddn3blggy78w4f21qa3chymz";
+ #     # sha256 = "1dmng7f5rv4hgd0b61chqx589ra7jajsrzw21n8gp8makw5khvb2";
+ #   }) { 
+ #     # Assuming you're on the same system architecture as your NixOS configuration
+ #      system = pkgs.system;
+ #      };
+ # in {
+ #   # Use Neovim from the unstable channel
+ #   environment.systemPackages = with unstable; [
+ #     neovim
+ #   ];
 
   # programs.neovim = {
   #   enable = true;
@@ -37,4 +36,4 @@ in
   #   viAlias = true;
   # };
 
-}
+
