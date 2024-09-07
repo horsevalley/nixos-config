6aa0d04dad238781f77266465df1fa5bb63b0902
Jonas Hestdahl <hestdahl@gmail.com>
2024-09-08 01:12:12 +0200

    fixing neovim sha256 hash

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -3,8 +3,8 @@
 let
   unstable = import (fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
-     sha256 = "1vc8bzz04ni7l15a9yd1x7jn0bw2b6rszg1krp6bcxyj3910pwb7";
-    # sha256 = "0s6h7r9jin9sd8l85hdjwl3jsvzkddn3blggy78w4f21qa3chymz";
+     # sha256 = "1vc8bzz04ni7l15a9yd1x7jn0bw2b6rszg1krp6bcxyj3910pwb7";
+    sha256 = "0s6h7r9jin9sd8l85hdjwl3jsvzkddn3blggy78w4f21qa3chymz";
 
   }) { 
     # Assuming you're on the same system architecture as your NixOS configuration
