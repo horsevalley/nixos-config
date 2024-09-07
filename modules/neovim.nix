0eca53886b6fefa3bd40958eadf38d4d70ab790f
Jonas Hestdahl <hestdahl@gmail.com>
2024-09-08 01:21:06 +0200

    fixing neovim sha256 hash

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -3,16 +3,16 @@
 let
   unstable = import (fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
-    # sha256 = "1vc8bzz04ni7l15a9yd1x7jn0bw2b6rszg1krp6bcxyj3910pwb7";
     sha256 = "0s6h7r9jin9sd8l85hdjwl3jsvzkddn3blggy78w4f21qa3chymz";
   }) { 
-    # Assuming you're on the same system architecture as your NixOS configuration
     system = pkgs.system;
   };
 in {
   # Use Neovim from the unstable channel
   environment.systemPackages = with unstable; [
     neovim
+    ripgrep # Useful for searching in Neovim
+    fd      # Optional, but handy for fuzzy finding in Neovim
   ];
 
   # Optionally, configure Neovim to be the default editor
@@ -23,7 +23,22 @@ in {
     viAlias = true;
   };
 
-  # You might want to add more Neovim specific configurations here, like plugins or custom settings
-  # For example, if you use a plugin manager like vim-plug or packer.nvim, you would configure it here or in your Neovim init file.
+  # Neovim Plugin Installation
+  system.activationScripts = {
+    installNeovimPlugins = ''
+      NVIM_PLUGIN_DIR="$HOME/.local/share/nvim/site/pack/packer/start"
+      if [ ! -d "$NVIM_PLUGIN_DIR" ]; then
+        mkdir -p "$NVIM_PLUGIN_DIR"
+        ${unstable.git}/bin/git clone --depth 1 https://github.com/wbthomason/packer.nvim \
+          "$NVIM_PLUGIN_DIR/packer.nvim"
+      fi
+    '';
+  };
+
+  # Add Neovim to PATH and set any necessary environment variables
+  environment.shellInit = ''
+    export PATH=$PATH:${unstable.neovim}/bin
+  '';
+
 }
 
