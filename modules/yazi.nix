8bfe92b2ed9f351d79b169cca55142320a0e649e
Jonas Hestdahl <hestdahl@gmail.com>
2024-09-08 17:31:05 +0200

    fixing yazi.nix

--- modules/yazi.nix
+++ modules/yazi.nix
@@ -12,30 +12,19 @@
 
   programs.yazi.enable = true;
 
-  system.activationScripts = {
-    installYaziPlugins = ''
-      YAZI_PLUGIN_DIR="$HOME/.config/yazi/plugins"
-      if [ ! -d "$YAZI_PLUGIN_DIR" ]; then
-        mkdir -p "$YAZI_PLUGIN_DIR"
-        if [ -d "/tmp/yazi" ]; then
-          rm -rf /tmp/yazi
-        fi
-        ${unstable.git}/bin/git clone https://github.com/sxyazi/yazi.git /tmp/yazi
-        if [ -d "/tmp/yazi/plugins" ]; then
-          cp -r /tmp/yazi/plugins/* "$YAZI_PLUGIN_DIR/"
-        fi
-        rm -rf /tmp/yazi
-      fi
-    '';
-  };
+  # Remove the activation script that was causing Yazi to start automatically
 
   environment.shellInit = ''
     export PATH=$PATH:${unstable.yazi}/bin
-    if [ -n "$BASH_VERSION" ]; then
-      eval "$(yazi init bash)"
-    elif [ -n "$ZSH_VERSION" ]; then
-      eval "$(yazi init zsh)"
-    fi
+  '';
+
+  # Add shell integration through shell rc files
+  environment.etc."bashrc.d/yazi-init.sh".text = ''
+    eval "$(yazi init bash)"
+  '';
+
+  environment.etc."zshrc.d/yazi-init.zsh".text = ''
+    eval "$(yazi init zsh)"
   '';
 
   # If you have a custom configuration file, you can add it like this:
