26550918022d1f6081ad70548f7212278607e0ff
Jonas Hestdahl <hestdahl@gmail.com>
2024-09-08 12:12:53 +0200

    adjusting fzf-history-widget aesthetics in zsh.nix

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -20,7 +20,6 @@ in
       bindkey -M menuselect 'j' vi-down-line-or-history
       # Load Starship prompt
       eval "$(starship init zsh)"
-
       # FZF settings
       # CTRL-/ to toggle small preview window to see the full command
       # CTRL-Y to copy the command into clipboard using pbcopy
@@ -30,7 +29,10 @@ in
         --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
         --color header:italic
         --history-size=1000000
-        --header 'Press CTRL-Y to copy command into clipboard'"
+        --header 'Press CTRL-Y to copy command into clipboard'
+        --height 40%
+        --layout=reverse
+        --width 80%"
       # Preview file content using bat (https://github.com/sharkdp/bat)
       export FZF_CTRL_T_OPTS="
         --walker-skip .git,node_modules,target
@@ -38,34 +40,15 @@ in
         --bind 'ctrl-/:change-preview-window(down|hidden|)'"
       export FZF_TMUX_OPTS="-p"
       export FZF_DEFAULT_COMMAND="fd --type f"
-
       # Load fzf
       if [ -n "''${commands[fzf-share]}" ]; then
         source "$(fzf-share)/key-bindings.zsh"
         source "$(fzf-share)/completion.zsh"
       fi
-
       # Explicitly bind Ctrl-R to fzf-history-widget
       bindkey '^R' fzf-history-widget
     '';
-    # shellInit = ''
-    #   # Your shell initialization code here
-    # '';
-    # loginShellInit = ''
-    #   # Your login shell initialization code here
-    # '';
-    # promptInit = "";  # We're using Starship, so we can leave this empty
   };
-
-  # Additional package dependencies
-  # environment.systemPackages = with pkgs; [
-  #   starship
-  #   fzf
-  #   fd
-  #   bat
-  #   # Add other packages you need
-  # ];
-
   # Enable Starship
-   programs.starship.enable = true;
+  programs.starship.enable = true;
 }
