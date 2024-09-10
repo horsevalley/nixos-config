a726658806f628c0c5d786c0c1af7e81218532b1
Jonas Hestdahl <hestdahl@gmail.com>
2024-09-10 13:12:12 +0200

    adding /home/jonash/.config structure to rofi.nix

--- modules/rofi.nix
+++ modules/rofi.nix
@@ -1,138 +1,144 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+  username = "yourusername"; # Replace with your actual username
+in
 {
-  home.packages = [ pkgs.rofi ];
+  environment.systemPackages = [ pkgs.rofi ];
 
-  xdg.configFile = {
-    "rofi/config.rasi".text = ''
-      configuration {
-        modi: "run,window,combi";
-        icon-theme: "Oranchelo";
-        show-icons: true;
-        terminal: "kitty";
-        drun-display-format: "{icon} {name}";
-        location: 0;
-        disable-history: false;
-        hide-scrollbar: true;
-        display-combi: " 🖥️  All ";
-        display-run: " 🏃  Run ";
-        display-window: " 🪟  Window";
-        sidebar-mode: true;
-      }
-      @theme "catppuccin-mocha"
-    '';
+  system.activationScripts.rofiConfig = lib.stringAfter [ "users" ] ''
+    mkdir -p /home/${username}/.config/rofi
+    cat > /home/${username}/.config/rofi/config.rasi << EOL
+configuration {
+    modi: "run,window,combi";
+    icon-theme: "Oranchelo";
+    show-icons: true;
+    terminal: "kitty";
+    drun-display-format: "{icon} {name}";
+    location: 0;
+    disable-history: false;
+    hide-scrollbar: true;
+    display-combi: " 🖥️  All ";
+    display-run: " 🏃  Run ";
+    display-window: " 🪟  Window";
+    sidebar-mode: true;
+}
+@theme "catppuccin-mocha"
+EOL
 
-    "rofi/catppuccin-mocha.rasi".text = ''
-      * {
-        bg-col:  #1e1e2e;
-        bg-col-light: #1e1e2e;
-        border-col: #1e1e2e;
-        selected-col: #1e1e2e;
-        blue: #89b4fa;
-        fg-col: #cdd6f4;
-        fg-col2: #f38ba8;
-        grey: #6c7086;
-        width: 600;
-        font: "JetBrainsMono Nerd Font 18";
-      }
+    cat > /home/${username}/.config/rofi/catppuccin-mocha.rasi << EOL
+* {
+    bg-col:  #1e1e2e;
+    bg-col-light: #1e1e2e;
+    border-col: #1e1e2e;
+    selected-col: #1e1e2e;
+    blue: #89b4fa;
+    fg-col: #cdd6f4;
+    fg-col2: #f38ba8;
+    grey: #6c7086;
+    width: 600;
+    font: "JetBrainsMono Nerd Font 18";
+}
 
-      element-text, element-icon , mode-switcher {
-        background-color: inherit;
-        text-color:       inherit;
-      }
+element-text, element-icon , mode-switcher {
+    background-color: inherit;
+    text-color:       inherit;
+}
 
-      window {
-        height: 360px;
-        border: 3px;
-        border-color: @border-col;
-        background-color: @bg-col;
-      }
+window {
+    height: 360px;
+    border: 3px;
+    border-color: @border-col;
+    background-color: @bg-col;
+}
 
-      mainbox {
-        background-color: @bg-col;
-      }
+mainbox {
+    background-color: @bg-col;
+}
 
-      inputbar {
-        children: [prompt,entry];
-        background-color: @bg-col;
-        border-radius: 5px;
-        padding: 2px;
-      }
+inputbar {
+    children: [prompt,entry];
+    background-color: @bg-col;
+    border-radius: 5px;
+    padding: 2px;
+}
 
-      prompt {
-        background-color: @blue;
-        padding: 6px;
-        text-color: @bg-col;
-        border-radius: 3px;
-        margin: 20px 0px 0px 20px;
-      }
+prompt {
+    background-color: @blue;
+    padding: 6px;
+    text-color: @bg-col;
+    border-radius: 3px;
+    margin: 20px 0px 0px 20px;
+}
 
-      textbox-prompt-colon {
-        expand: false;
-        str: ":";
-      }
+textbox-prompt-colon {
+    expand: false;
+    str: ":";
+}
 
-      entry {
-        padding: 6px;
-        margin: 20px 0px 0px 10px;
-        text-color: @fg-col;
-        background-color: @bg-col;
-      }
+entry {
+    padding: 6px;
+    margin: 20px 0px 0px 10px;
+    text-color: @fg-col;
+    background-color: @bg-col;
+}
 
-      listview {
-        border: 0px 0px 0px;
-        padding: 6px 0px 0px;
-        margin: 10px 0px 0px 20px;
-        columns: 2;
-        lines: 5;
-        background-color: @bg-col;
-      }
+listview {
+    border: 0px 0px 0px;
+    padding: 6px 0px 0px;
+    margin: 10px 0px 0px 20px;
+    columns: 2;
+    lines: 5;
+    background-color: @bg-col;
+}
 
-      element {
-        padding: 5px;
-        background-color: @bg-col;
-        text-color: @fg-col  ;
-      }
+element {
+    padding: 5px;
+    background-color: @bg-col;
+    text-color: @fg-col  ;
+}
 
-      element-icon {
-        size: 25px;
-      }
+element-icon {
+    size: 25px;
+}
 
-      element selected {
-        background-color:  @selected-col ;
-        text-color: @fg-col2  ;
-      }
+element selected {
+    background-color:  @selected-col ;
+    text-color: @fg-col2  ;
+}
 
-      mode-switcher {
-        spacing: 0;
-      }
+mode-switcher {
+    spacing: 0;
+}
 
-      button {
-        padding: 10px;
-        background-color: @bg-col-light;
-        text-color: @grey;
-        vertical-align: 0.5; 
-        horizontal-align: 0.5;
-      }
+button {
+    padding: 10px;
+    background-color: @bg-col-light;
+    text-color: @grey;
+    vertical-align: 0.5; 
+    horizontal-align: 0.5;
+}
 
-      button selected {
-        background-color: @bg-col;
-        text-color: @blue;
-      }
+button selected {
+    background-color: @bg-col;
+    text-color: @blue;
+}
 
-      message {
-        background-color: @bg-col-light;
-        margin: 2px;
-        padding: 2px;
-        border-radius: 5px;
-      }
+message {
+    background-color: @bg-col-light;
+    margin: 2px;
+    padding: 2px;
+    border-radius: 5px;
+}
 
-      textbox {
-        padding: 6px;
-        margin: 20px 0px 0px 20px;
-        text-color: @blue;
-        background-color: @bg-col-light;
-      }
-    '';
-  };
+textbox {
+    padding: 6px;
+    margin: 20px 0px 0px 20px;
+    text-color: @blue;
+    background-color: @bg-col-light;
+}
+EOL
+
+    chown -R ${username}:users /home/${username}/.config/rofi
+  '';
 }
