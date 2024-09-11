225d9d5f40731b7392c2ddde2844c7a44e7463db
Jonas Hestdahl <hestdahl@gmail.com>
2024-09-11 20:38:18 +0200

    added grub bootlader for x99-5930k-nixos

--- hosts/x99-5930k/default.nix
+++ hosts/x99-5930k/default.nix
@@ -5,13 +5,26 @@
     ./hardware-configuration.nix
   ];
   networking.hostName = "x99-5930k-nixos";
-  # File systems. use systemdmount instead of fstab
 
   swapDevices = [
   { device = "/swapfile";
     size = 16*1024;
   }
 ];
+
+  # use GRUB bootloader instead of systemd
+  boot.loader = {
+  grub = {
+    enable = true;
+    device = "nodev";  # Use this for UEFI systems
+    efiSupport = true;
+    useOSProber = true;  # Optional: enables OS prober to find other operating systems
+  };
+  efi = {
+    canTouchEfiVariables = true;
+    efiSysMountPoint = "/boot";
+  };
+};
 
   system.stateVersion = "24.05";
 }
