b8a11175e8035766be16a46afdf139ec4074fb32
Jonas Hestdahl <hestdahl@gmail.com>
2024-09-12 16:34:22 +0200

    disabled systemActivationScript for librewolf.nix

--- modules/librewolf.nix
+++ modules/librewolf.nix
@@ -10,32 +10,32 @@ in
   ];
 
   # Set up Arkenfox user.js for each user
-  system.activationScripts.setupLibrewolf = ''
-    for user in /home/*; do
-      if [ -d "$user" ]; then
-        username=$(basename "$user")
-        librewolf_dir="/home/$username/.librewolf"
-        mkdir -p "$librewolf_dir"
-        chown $username:users "$librewolf_dir"
-        
-        # Find or create default profile
-        default_profile=$(find "$librewolf_dir" -maxdepth 1 -type d -name "*.default" | head -n 1)
-        if [ -z "$default_profile" ]; then
-          default_profile="$librewolf_dir/$(date +%s).default"
-          mkdir -p "$default_profile"
-          chown $username:users "$default_profile"
-        fi
-        
-        # Download Arkenfox user.js
-        if ${pkgs.curl}/bin/curl -o "$default_profile/user.js" ${arkenfoxUrl}; then
-          chown $username:users "$default_profile/user.js"
-          echo "LibreWolf setup complete for user $username"
-        else
-          echo "Error: Failed to download Arkenfox user.js for user $username" >&2
-        fi
-      fi
-    done
-  '';
+  # system.activationScripts.setupLibrewolf = ''
+  #   for user in /home/*; do
+  #     if [ -d "$user" ]; then
+  #       username=$(basename "$user")
+  #       librewolf_dir="/home/$username/.librewolf"
+  #       mkdir -p "$librewolf_dir"
+  #       chown $username:users "$librewolf_dir"
+  #       
+  #       # Find or create default profile
+  #       default_profile=$(find "$librewolf_dir" -maxdepth 1 -type d -name "*.default" | head -n 1)
+  #       if [ -z "$default_profile" ]; then
+  #         default_profile="$librewolf_dir/$(date +%s).default"
+  #         mkdir -p "$default_profile"
+  #         chown $username:users "$default_profile"
+  #       fi
+  #       
+  #       # Download Arkenfox user.js
+  #       if ${pkgs.curl}/bin/curl -o "$default_profile/user.js" ${arkenfoxUrl}; then
+  #         chown $username:users "$default_profile/user.js"
+  #         echo "LibreWolf setup complete for user $username"
+  #       else
+  #         echo "Error: Failed to download Arkenfox user.js for user $username" >&2
+  #       fi
+  #     fi
+  #   done
+  # '';
 
   # Enable Wayland support
   environment.variables = {
