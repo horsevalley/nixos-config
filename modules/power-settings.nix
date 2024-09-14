630e3a62897f4da9981b20b51d9cee06391891d4
Jonas Hestdahl <hestdahl@gmail.com>
2024-09-14 23:48:22 +0200

    added power-settings.nix

---
+++ modules/power-settings.nix
@@ -0,0 +1,47 @@
+# modules/power-settings.nix
+{ config, pkgs, lib, ... }:
+
+let
+  # Define settings for laptops
+  laptopSettings = {
+    # Power management settings for laptops
+    services.logind = {
+      lidSwitch = "suspend";
+      lidSwitchDocked = "ignore";
+      handleLidSwitch = "suspend";
+      handleLidSwitchExternalPower = "ignore";
+      handleSuspendKey = "suspend";
+      handleHibernateKey = "hibernate";
+      idleAction = "suspend";
+      idleActionDelaySec = 1800; # 30 minutes
+    };
+
+    # Enable TLP for advanced power management (optional)
+    services.tlp.enable = true;
+
+    # CPU frequency scaling governor
+    powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+
+    # Enable ACPI event daemon
+    services.acpid.enable = true;
+  };
+
+  # Define settings for desktops
+  desktopSettings = {
+    # Power management settings for desktops
+    services.logind = {
+      lidSwitch = "ignore";
+      handleLidSwitch = "ignore";
+      handleSuspendKey = "ignore";
+      handleHibernateKey = "ignore";
+      idleAction = "ignore";
+    };
+
+    # CPU frequency scaling governor
+    powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
+  };
+in
+{
+  # Apply settings based on whether the system is a laptop or desktop
+  (if config.hardware.isLaptop then laptopSettings else desktopSettings)
+}
