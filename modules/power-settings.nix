d86f34d914d5b3055071f96ac15713f0ca8403e4
Jonas Hestdahl <hestdahl@gmail.com>
2024-09-15 00:08:57 +0200

    fixing power-settings.nix

--- modules/power-settings.nix
+++ modules/power-settings.nix
@@ -1,45 +1,54 @@
 # modules/power-settings.nix
 { config, pkgs, lib, ... }:
 
+let
+  ##### Laptop-Specific Settings #####
+  laptopSettings = {
+    services.logind = {
+      lidSwitch = lib.mkForce "suspend";
+      lidSwitchDocked = lib.mkForce "ignore";
+      handleLidSwitch = lib.mkForce "suspend";
+      handleSuspendKey = lib.mkForce "suspend";
+      # Uncomment if 'handleHibernateKey' exists
+      # handleHibernateKey = lib.mkForce "hibernate";
+      idleAction = lib.mkForce "suspend";
+      idleActionDelaySec = 1800; # 30 minutes
+    };
+
+    services.tlp.enable = true;
+    powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+    services.acpid.enable = true;
+  };
+
+  ##### Desktop-Specific Settings #####
+  desktopSettings = {
+    services.logind = {
+      lidSwitch = lib.mkForce "ignore";
+      handleLidSwitch = lib.mkForce "ignore";
+      handleSuspendKey = lib.mkForce "ignore";
+      # Uncomment if 'handleHibernateKey' exists
+      # handleHibernateKey = lib.mkForce "ignore";
+      idleAction = lib.mkForce "ignore";
+    };
+
+    powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
+  };
+in
 {
-  # Common settings can go here if any
+  ##### Define Custom Option #####
+  options = {
+    myConfig.isLaptop = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Set to true if the system is a laptop";
+    };
+  };
+
+  ##### Conditionally Include Configurations #####
+  # Merge laptop settings if 'myConfig.isLaptop' is true
+  config = lib.mkIf config.myConfig.isLaptop laptopSettings
+
+  # Merge desktop settings if 'myConfig.isLaptop' is false
+    // lib.mkIf (!config.myConfig.isLaptop) desktopSettings;
 }
-
-# Merge the laptop settings if the system is a laptop
-// (lib.mkIf config.hardware.isLaptop {
-  # Laptop-specific settings
-  services.logind = {
-    lidSwitch = "suspend";
-    lidSwitchDocked = "ignore";
-    handleLidSwitch = "suspend";
-    handleSuspendKey = "suspend";
-    handleHibernateKey = "hibernate";
-    idleAction = "suspend";
-    idleActionDelaySec = 1800; # 30 minutes
-  };
-
-  # Enable TLP for advanced power management (optional)
-  services.tlp.enable = true;
-
-  # CPU frequency scaling governor
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-
-  # Enable ACPI event daemon
-  services.acpid.enable = true;
-})
-
-# Merge the desktop settings if the system is not a laptop
-// (lib.mkIf (!config.hardware.isLaptop) {
-  # Desktop-specific settings
-  services.logind = {
-    lidSwitch = "ignore";
-    handleLidSwitch = "ignore";
-    handleSuspendKey = "ignore";
-    handleHibernateKey = "ignore";
-    idleAction = "ignore";
-  };
-
-  # CPU frequency scaling governor
-  powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
-})
 
