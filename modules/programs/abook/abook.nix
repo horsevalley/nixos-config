64abd0f44f3b70d5e5f35f821d1e50336e75c3d1
Jonas Hestdahl <hestdahl@gmail.com>
2024-09-17 12:33:09 +0200

    updated abook.nix

--- modules/programs/abook/abook.nix
+++ modules/programs/abook/abook.nix
@@ -1,50 +1,19 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
   cfg = config.programs.abook;
-
-  # Custom abook package with patched default directory
-  customAbook = pkgs.abook.overrideAttrs (oldAttrs: {
-    patches = (oldAttrs.patches or []) ++ [
-      (pkgs.writeText "abook-config-dir.patch" ''
-        diff --git a/abook.c b/abook.c
-        index xxxxxxx..yyyyyyy 100644
-        --- a/abook.c
-        +++ b/abook.c
-        @@ -xxx,7 +xxx,7 @@ init_abook()
-         {
-         	char *home = getenv("HOME");
-         	
-        -	snprintf(rcfile, sizeof(rcfile), "%s/.abook/abookrc", home);
-        +	snprintf(rcfile, sizeof(rcfile), "%s/.config/abook/abookrc", home);
-         	
-         	init_opts();
-         	
-        @@ -yyy,7 +yyy,7 @@ init_abook()
-         		strncpy(rcfile, CFG_FILE, sizeof(rcfile));
-         	
-         	if(!database) {
-        -		snprintf(datafile, sizeof(datafile), "%s/.abook/addressbook", home);
-        +		snprintf(datafile, sizeof(datafile), "%s/.config/abook/addressbook", home);
-         		database = xstrdup(datafile);
-         	}
-         }
-      '')
-    ];
-  });
+  username = "jonash"; # Replace with your actual username
 
   # Function to convert a CSV line to abook format
   convertCsvToAbookEntry = line:
     let
-      fields = splitString "," line;
-      name = elemAt fields 0;
-      email = elemAt fields 4;
-      phone = elemAt fields 5;
-      address = elemAt fields 7;
-      organization = elemAt fields 8;
-      notes = elemAt fields 9;
+      fields = builtins.split "," line;
+      name = builtins.elemAt fields 0;
+      email = builtins.elemAt fields 4;
+      phone = builtins.elemAt fields 5;
+      address = builtins.elemAt fields 7;
+      organization = builtins.elemAt fields 8;
+      notes = builtins.elemAt fields 9;
     in
     ''
       [${name}]
@@ -60,45 +29,53 @@ let
   abookData = 
     let
       csvContent = builtins.readFile ./cleaned_contacts.csv;
-      csvLines = splitString "\n" csvContent;
+      csvLines = builtins.filter (line: line != "") (builtins.split "\n" csvContent);
       # Skip the header line
-      dataLines = tail csvLines;
+      dataLines = builtins.tail csvLines;
     in
-    concatMapStrings convertCsvToAbookEntry dataLines;
-
-  # Create the abook database file
-  abookDbFile = pkgs.writeText "addressbook" ''
-    # abook addressbook file
-
-    [format]
-    program=abook
-    version=0.6.1
-
-    ${abookData}
-  '';
-
-  # Create the abook configuration file
-  abookConfigFile = pkgs.writeText "abookrc" ''
-    set show_all_emails=true
-    set add_email_prevent_duplicates=true
-    set autosave=true
-    set www_command=xdg-open
-    set use_mouse=true
-  '';
+    builtins.concatStringsSep "\n" (map convertCsvToAbookEntry dataLines);
 
 in {
   options.programs.abook = {
-    enable = mkEnableOption "abook address book manager";
+    enable = lib.mkEnableOption "abook address book manager";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.abook;
+      description = "The abook package to use.";
+    };
   };
 
-  config = mkIf cfg.enable {
-    environment.systemPackages = [ customAbook ];
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [ cfg.package ];
 
-    system.activationScripts.abookSetup = ''
-      mkdir -p $HOME/.config/abook
-      cp ${abookDbFile} $HOME/.config/abook/addressbook
-      cp ${abookConfigFile} $HOME/.config/abook/abookrc
-      chmod 600 $HOME/.config/abook/addressbook $HOME/.config/abook/abookrc
+    environment.etc."abook/addressbook".text = ''
+      # abook addressbook file
+
+      [format]
+      program=abook
+      version=0.6.1
+
+      ${abookData}
     '';
+
+    environment.etc."abook/abookrc".text = ''
+      set show_all_emails=true
+      set add_email_prevent_duplicates=true
+      set autosave=true
+      set www_command=xdg-open
+      set use_mouse=true
+    '';
+
+    system.activationScripts.abook-setup = ''
+      mkdir -p /home/${username}/.config/abook
+      cp /etc/abook/addressbook /home/${username}/.config/abook/addressbook
+      cp /etc/abook/abookrc /home/${username}/.config/abook/abookrc
+      chown -R ${username}:users /home/${username}/.config/abook
+    '';
+
+    environment.sessionVariables = {
+      ABOOK_CONFIG = "$HOME/.config/abook/abookrc";
+    };
   };
 }
