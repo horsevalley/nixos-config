ab51cad9b662f4895d5ea65f4b2e142dfcf30af6
Jonas Hestdahl <hestdahl@gmail.com>
2024-09-18 20:44:17 +0200

    changed neomutt keybinds, settings and aesthetics

--- modules/mail/neomutt.nix
+++ modules/mail/neomutt.nix
@@ -15,39 +15,35 @@ in
     
     # Create neomutt config file
     cat > /home/${username}/.config/neomutt/neomuttrc << EOL
-    # Basic Settings
+    # vim: filetype=neomuttrc
+
+    # General Settings
     set mailcap_path = ~/.config/neomutt/mailcap
-    set date_format="%a %d %b %H:%M"
+    set date_format="%d/%m/%y %H:%M"
     set index_format="%2C %Z %?X?A& ? %D %-15.15F %s (%-4.4c)"
-    set sort = 'threads'
-    set sort_aux = 'reverse-last-date-received'
-    set uncollapse_jump
-    set sort_re
-    set reply_regexp = "^(([Rr][Ee]?(\[[0-9]+\])?: *)?(\[[^]]+\] *)?)*"
-    set quote_regexp = "^( {0,4}[>|:#%]| {0,4}[a-z0-9]+[>|]+)+"
-    set send_charset = "utf-8"
-    set charset = "utf-8"
-    set editor = "vim"
+    set sort = 'reverse-date'
+    set markers = no
+    set mark_old = no
+    set mime_forward = yes
+    set smtp_authenticators = 'gssapi:login'
+    set wait_key = no
+    set rfc2047_parameters = yes
+    set sleep_time = 0
+    set markers = no
 
-    # Vim-like keybindings
-    bind index,pager g noop
-    bind index gg first-entry
-    bind index G last-entry
-    bind pager gg top
-    bind pager G bottom
-    bind index,pager \Cd half-down
-    bind index,pager \Cu half-up
-    bind index,pager N search-opposite
-    bind index L limit
-    bind pager L exit
+    # Ensure TLS is enforced
+    set ssl_starttls = yes
+    set ssl_force_tls = yes
 
-    # Use Vim keys in menus
-    bind generic,index,pager k previous-entry
-    bind generic,index,pager j next-entry
-    bind generic,index,pager \Ck previous-page
-    bind generic,index,pager \Cj next-page
-    bind generic,index,pager,browser gg first-entry
-    bind generic,index,pager,browser G last-entry
+    # Sidebar settings
+    set sidebar_visible = no
+    set sidebar_width = 20
+    set sidebar_short_path = yes
+    set sidebar_next_new_wrap = yes
+    set mail_check_stats
+
+    auto_view text/html
+    alternative_order text/plain text/enriched text/html
 
     # Pager View Options
     set pager_index_lines = 10
@@ -56,12 +52,6 @@ in
     set menu_scroll
     set tilde
     unset markers
-
-    # Email Headers
-    ignore *
-    unignore from: to: cc: date: subject:
-    unhdr_order *
-    hdr_order from: to: cc: date: subject:
 
     # Compose View Options
     set envelope_from                    # which from?
@@ -76,38 +66,110 @@ in
     set reverse_name                     # reply as whomever it was to
     set include                          # include message in replies
     set forward_quote                    # include message in forwards
+    set text_flowed
+    set editor = "nvim"
+    set sig_dashes = no
+    set fast_reply = yes
+    set sendmail_wait = 0
+    set reply_regexp = "^(([Rr][Ee]?(\[[0-9]+\])?: *)?(\[[^]]+\] *)?)*"
 
-    macro index,pager a "<enter-command>set my_pipe_decode=\$pipe_decode pipe_decode<return><pipe-message>abook --add-email<return><enter-command>set pipe_decode=\$my_pipe_decode; unset my_pipe_decode<return>" "add the sender address to abook"
-    macro index \Cr "T~U<enter><tag-prefix><clear-flag>N<untag-pattern>.<enter>" "mark all messages as read"
-    macro index O "<shell-escape>mailsync<enter>" "run mailsync to sync all mail"
-    macro index \Cf "<enter-command>unset wait_key<enter><shell-escape>printf 'Enter a search term to find with notmuch: '; read x; echo \$x >\"\${XDG_CACHE_HOME:-\$HOME/.cache}/mutt_terms\"<enter><limit>~i \"\`notmuch search --output=messages \$(cat \"\${XDG_CACHE_HOME:-\$HOME/.cache}/mutt_terms\") | head -n 600 | perl -le '@a=<>;s/\^id:// for@a;$,=\"|\";print@a' | perl -le '@a=<>; chomp@a; s/\\+/\\\\+/g for@a; s/\\$/\\\\\\$/g for@a;print@a' \`\"<enter>" "show only messages matching a notmuch pattern"
-    macro index A "<limit>all\n" "show all messages (undo limit)"
+    # Status Bar
+    set status_chars  = " *%A"
+    set status_format = "───[ Folder: %f ]───[%r%m messages%?n? (%n new)?%?d? (%d to delete)?%?t? (%t tagged)? ]───%>─%?p?( %p postponed )?───"
 
-    # Sidebar mappings
-    set sidebar_visible = no
-    set sidebar_width = 20
-    set sidebar_short_path = yes
-    set sidebar_next_new_wrap = yes
-    set mail_check_stats
-    set sidebar_format = '%D%?F? [%F]?%* %?N?%N/? %?S?%S?'
-    bind index,pager \Ck sidebar-prev
-    bind index,pager \Cj sidebar-next
-    bind index,pager \Co sidebar-open
-    bind index,pager \Cp sidebar-prev-new
-    bind index,pager \Cn sidebar-next-new
-    bind index,pager B sidebar-toggle-visible
+    # Key Bindings
+    bind index,pager g noop
+    bind index gg first-entry
+    bind index G last-entry
+    bind pager gg top
+    bind pager G bottom
+    bind index,pager \Cd half-down
+    bind index,pager \Cu half-up
+    bind index,pager N search-opposite
+    bind index L limit
+    bind pager L exit
 
-    # Colors
-    color normal        default         default
-    color index         color4          default         ~N # new messages
-    color index         color1          default         ~F # flagged messages
-    color index         color3          default         ~T # tagged messages
-    color index         color1          default         ~D # deleted messages
-    color body          color2          default         "(https?|ftp)://[\-\.,/%~_:?&=\#a-zA-Z0-9]+" # URLs
-    color body          color2          default         "[\-\.+_a-zA-Z0-9]+@[\-\.a-zA-Z0-9]+" # email addresses
-    color attachment    color5          default
-    color signature     color8          default
-    color search        color11         default
+    # Sidebar bindings
+    bind index,pager \CP sidebar-prev
+    bind index,pager \CN sidebar-next
+    bind index,pager \CO sidebar-open
+
+    # AESTHETICS
+
+    # Default index colors:
+    color index_number blue default
+    color index blue default '.*'
+    color index_author brightblue default '.*'
+    color index_subject brightblue default '.*'
+
+    # New mail is boldened:
+    color index brightwhite default "~N"
+    color index_author brightwhite default "~N"
+    color index_subject brightwhite default "~N"
+
+    # Tagged mail is highlighted:
+    color index brightyellow blue "~T"
+    color index_author brightred blue "~T"
+    color index_subject brightcyan blue "~T"
+
+    # Flagged mail is highlighted:
+    color index brightgreen default "~F"
+    color index_subject brightgreen default "~F"
+    color index_author brightgreen default "~F"
+
+    # Other colors and aesthetic settings:
+    mono bold bold
+    mono underline underline
+    mono indicator reverse
+    mono error bold
+    color normal default default
+    color indicator brightblack white
+    color sidebar_highlight red default
+    color sidebar_divider brightblack black
+    color sidebar_flagged red black
+    color sidebar_new green black
+    color error red default
+    color tilde black default
+    color message cyan default
+    color markers red white
+    color attachment white default
+    color search brightmagenta default
+    color status brightwhite blue
+    color hdrdefault brightgreen default
+    color quoted green default
+    color quoted1 blue default
+    color quoted2 cyan default
+    color quoted3 yellow default
+    color quoted4 red default
+    color quoted5 brightred default
+    color signature brightgreen default
+    color bold black default
+    color underline black default
+
+    # Regex highlighting:
+    color header brightmagenta default "^From"
+    color header brightcyan default "^Subject"
+    color header brightwhite default "^(CC|BCC)"
+    color header blue default ".*"
+    color body brightred default "[\-\.+_a-zA-Z0-9]+@[\-\.a-zA-Z0-9]+" # Email addresses
+    color body brightblue default "(https?|ftp)://[\-\.,/%~_:?&=\#a-zA-Z0-9]+" # URL
+    color body green default "\`[^\`]*\`" # Green text between ` and `
+    color body brightblue default "^# \.*" # Headings as bold blue
+    color body brightcyan default "^## \.*" # Subheadings as bold cyan
+    color body brightgreen default "^### \.*" # Subsubheadings as bold green
+    color body yellow default "^(\t| )*(-|\\*) \.*" # List items as yellow
+    color body brightcyan default "[;:][-o][)/(|]" # emoticons
+    color body brightcyan default "[;:][)(|]" # emoticons
+    color body brightcyan default "[ ][*][^*]*[*][ ]?" # more emoticon?
+    color body brightcyan default "[ ]?[*][^*]*[*][ ]" # more emoticon?
+    color body red default "(BAD signature)"
+    color body cyan default "(Good signature)"
+    color body brightblack default "^gpg: Good signature .*"
+    color body brightyellow default "^gpg: "
+    color body brightyellow red "^gpg: BAD signature from.*"
+    mono body bold "^gpg: Good signature"
+    mono body bold "^gpg: BAD signature from.*"
+    color body red default "([a-z][a-z0-9+-]*://(((([a-z0-9_.!~*'();:&=+$,-]|%[0-9a-f][0-9a-f])*@)?((([a-z0-9]([a-z0-9-]*[a-z0-9])?)\\.)*([a-z]([a-z0-9-]*[a-z0-9])?)\\.?|[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+)(:[0-9]+)?)|([a-z0-9_.!~*'()$,;:@&=+-]|%[0-9a-f][0-9a-f])+)(/([a-z0-9_.!~*'():@&=+$,-]|%[0-9a-f][0-9a-f])*(;([a-z0-9_.!~*'():@&=+$,-]|%[0-9a-f][0-9a-f])*)*(/([a-z0-9_.!~*'():@&=+$,-]|%[0-9a-f][0-9a-f])*(;([a-z0-9_.!~*'():@&=+$,-]|%[0-9a-f][0-9a-f])*)*)*)?(\\?([a-z0-9_.!~*'();/?:@&=+$,-]|%[0-9a-f][0-9a-f])*)?(#([a-z0-9_.!~*'();/?:@&=+$,-]|%[0-9a-f][0-9a-f])*)?|(www|ftp)\\.(([a-z0-9]([a-z0-9-]*[a-z0-9])?)\\.)*([a-z]([a-z0-9-]*[a-z0-9])?)\\.?(:[0-9]+)?(/([-a-z0-9_.!~*'():@&=+$,]|%[0-9a-f][0-9a-f])*(;([-a-z0-9_.!~*'():@&=+$,]|%[0-9a-f][0-9a-f])*)*(/([-a-z0-9_.!~*'():@&=+$,]|%[0-9a-f][0-9a-f])*(;([-a-z0-9_.!~*'():@&=+$,]|%[0-9a-f][0-9a-f])*)*)*)?(\\?([-a-z0-9_.!~*'();/?:@&=+$,]|%[0-9a-f][0-9a-f])*)?(#([-a-z0-9_.!~*'();/?:@&=+$,]|%[0-9a-f][0-9a-f])*)?)[^].,:;!)? \t\r\n<>\"]"
 
     # Vim-like macros
     macro index,pager gi "<change-folder>=INBOX<enter>" "go to inbox"
@@ -148,8 +210,11 @@ in
     # Create mailcap file
     cat > /home/${username}/.config/neomutt/mailcap << EOL
     text/html; ${pkgs.lynx}/bin/lynx -dump -force_html %s; copiousoutput; description=HTML Text; nametemplate=%s.html
+    text/plain; ${pkgs.vim}/bin/vim %s
     application/pdf; ${pkgs.zathura}/bin/zathura %s; test=test -n "$DISPLAY"
     image/*; ${pkgs.feh}/bin/feh %s; test=test -n "$DISPLAY"
+    video/*; ${pkgs.mpv}/bin/mpv --quiet %s; test=test -n "$DISPLAY"
+    audio/*; ${pkgs.mpv}/bin/mpv --quiet %s
     EOL
 
     # Set correct permissions
