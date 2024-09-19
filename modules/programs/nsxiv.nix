bb69fc08805ca8f8f51ea234dbfd3c88d57d0f40
Jonas Hestdahl <hestdahl@gmail.com>
2024-09-19 23:00:23 +0200

    added programs/nsxiv.nix

--- modules/programs/nsxiv.nix
+++ modules/programs/nsxiv.nix
@@ -1,62 +1,63 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Enable nsxiv
-  programs.nsxiv = {
-    enable = true;
+  # Install nsxiv
+  environment.systemPackages = [ pkgs.nsxiv ];
 
-    # Configuration options for nsxiv
-    settings = {
-      # Set background color to black
-      background = "#000000";
+  # Create a custom wrapper for nsxiv with our desired configuration
+  environment.systemPackages = with pkgs; [
+    (writeShellScriptBin "nsxiv-custom" ''
+      #!/usr/bin/env sh
+      export NSXIV_BACKGROUND="#000000"  # Set background color to black
+      
+      # Create a temporary directory for the key handler script
+      TEMP_DIR=$(mktemp -d)
+      trap 'rm -rf "$TEMP_DIR"' EXIT
 
-      # Vim-like keybindings for image navigation
-      keyhandler = pkgs.writeShellScript "nsxiv-keyhandler" ''
-        #!usr/bin/env sh
-        while read -r file
-        do
-          case "$1" in
-            # Navigation
-            "j"|"Down")     echo "next" ;;        # Next image
-            "k"|"Up")       echo "prev" ;;        # Previous image
-            "h"|"Left")     echo "scroll_left" ;; # Scroll left
-            "l"|"Right")    echo "scroll_right" ;;# Scroll right
+      # Create the key handler script
+      cat > "$TEMP_DIR/nsxiv-key-handler" << EOF
 
-            # Zoom
-            # "="|"+")        echo "zoom_in" ;;     # Zoom in
-            # "-")            echo "zoom_out" ;;    # Zoom out
-            "K"|"+")        echo "zoom_in" ;;     # Zoom in
-            "J")            echo "zoom_out" ;;    # Zoom out
+      #!/usr/bin/env sh
+      while read -r file
+      do
+        case "$1" in
+          # Navigation
+          "j"|"Down")     echo "next" ;;        # Next image
+          "k"|"Up")       echo "prev" ;;        # Previous image
+          "h"|"Left")     echo "scroll_left" ;; # Scroll left
+          "l"|"Right")    echo "scroll_right" ;;# Scroll right
 
-            # Rotation
-            "r")            echo "rotate" ;;      # Rotate clockwise
-            "R")            echo "rotate_cc" ;;   # Rotate counter-clockwise
+          # Zoom
+          "="|"+")        echo "zoom_in" ;;     # Zoom in
+          "-")            echo "zoom_out" ;;    # Zoom out
 
-            # Flipping
-            "f")            echo "flip" ;;        # Flip horizontally
-            "F")            echo "flip_v" ;;      # Flip vertically
+          # Rotation
+          "r")            echo "rotate" ;;      # Rotate clockwise
+          "R")            echo "rotate_cc" ;;   # Rotate counter-clockwise
 
-            # File operations
-            "d")            echo "remove" ;;      # Mark for deletion
-            "D")            echo "delete" ;;      # Delete marked files
+          # Flipping
+          "f")            echo "flip" ;;        # Flip horizontally
+          "F")            echo "flip_v" ;;      # Flip vertically
 
-            # Other
-            "m")            echo "mark" ;;        # Mark/unmark current image
-            "q")            echo "quit" ;;        # Quit nsxiv
+          # File operations
+          "d")            echo "remove" ;;      # Mark for deletion
+          "D")            echo "delete" ;;      # Delete marked files
 
-            # Add more custom keybindings here
-            # "key")         echo "action" ;;
-          esac
-        done
-      '';
-    };
+          # Other
+          "m")            echo "mark" ;;        # Mark/unmark current image
+          "q")            echo "quit" ;;        # Quit nsxiv
 
-    # Additional options
-    extraOptions = [
-      "-z" "100"  # Set initial zoom level to 100%
-      "-a"        # Make nsxiv anti-aliased
-    ];
-  };
+          # Add more custom keybindings here
+          # "key")         echo "action" ;;
+        esac
+      done
+      EOF
 
-  # You can add more nsxiv-related configurations here if needed
+      chmod +x "$TEMP_DIR/nsxiv-key-handler"
+
+      # Run nsxiv with our custom configuration
+      ${nsxiv}/bin/nsxiv -a -z 100 -k "$TEMP_DIR/nsxiv-key-handler" "$@"
+    '')
+  ];
+
 }
