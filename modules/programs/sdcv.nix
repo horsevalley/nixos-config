47542992602985be08d948b214987e5e951c91b5
Jonas Hestdahl <hestdahl@gmail.com>
2024-10-03 11:42:41 +0200

    fixing sdcv.nix

--- modules/programs/sdcv.nix
+++ modules/programs/sdcv.nix
@@ -3,21 +3,20 @@
 let
   sdcvConfig = pkgs.writeText "sdcvrc" ''
     # SDCV configuration
-    # You can add any sdcv-specific configurations here
-    # For example:
-     use-dict "WordNet"
-    # use-dict "Another Dictionary"
+    use-dict "WordNet"
   '';
+
+  localDictPath = "/home/jonash/Downloads/stardict/dic/stardict-dictd_www.dict.org_wn-2.4.2";
 in
 {
   environment.systemPackages = [ 
     (pkgs.sdcv.overrideAttrs (oldAttrs: rec {
-      version = "0.5.5";  # You can update this to a newer version if available
+      version = "0.5.5";
       src = pkgs.fetchFromGitHub {
         owner = "Dushistov";
         repo = "sdcv";
         rev = "v${version}";
-        sha256 = "1cg9g30i59wd8vn2xrhxgv0wdgvpgxycyifqmchjpcc3qadcv5h2";  # You may need to update this
+        sha256 = "sha256-EyvljVXhOsdxIYOGTzD+T16nvW7/RNx3DuQ2OdhjXJ4=";
       };
     }))
   ];
@@ -28,6 +27,10 @@ in
     sdcvConfig = ''
       mkdir -p $HOME/.config
       ln -sf ${sdcvConfig} $HOME/.config/sdcvrc
+    '';
+    linkWordNetDict = ''
+      mkdir -p /etc/stardict/dic
+      ln -sf ${localDictPath} /etc/stardict/dic/WordNet
     '';
   };
 }
