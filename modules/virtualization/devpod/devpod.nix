8ec05ae9c16a2a800e5189bccd6265419b3c9305
Jonas Hestdahl <hestdahl@gmail.com>
2024-10-29 12:37:48 +0100

    fixing devpod.nix

--- modules/virtualization/devpod/devpod.nix
+++ modules/virtualization/devpod/devpod.nix
@@ -1,15 +1,6 @@
 { config, pkgs, unstable, ... }:
 
 {
-  environment.systemPackages = [
-    (unstable.devpod.override {
-    })
-  ];
-
-  environment.variables = {
-    EDITOR = "vim";
-    VISUAL = "vim";
-  };
 
   # Force the system to use the unstable version
   nixpkgs.overlays = [
