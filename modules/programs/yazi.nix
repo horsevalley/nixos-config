bc731508d8d4b64be3d4e3693408b24fcb68cf83
Jonas Hestdahl <hestdahl@gmail.com>
2024-11-03 14:45:48 +0100

    fixing yazi.nix

--- modules/programs/yazi.nix
+++ modules/programs/yazi.nix
@@ -1,6 +1,23 @@
 { config, pkgs, unstable, ... }:
+let
+  final = pkgs.extend (final: prev: {
+    _7zz = prev._7zz.override {
+      enableAsm = false;
+      useSystemAsm = false;
+      useMasm = false;
+    };
+    yazi = (unstable.yazi.override {
+      inherit (_7zz);
+    }).overrideAttrs (old: {
+      buildInputs = (old.buildInputs or []) ++ [
+        final._7zz
+      ];
+    });
+  });
+in
 {
   environment.systemPackages = with pkgs; [
+    final.yazi
     file
     imagemagick
     kitty
@@ -8,18 +25,7 @@
 
   nixpkgs.overlays = [
     (final: prev: {
-      _7zz = prev._7zz.override {
-        enableAsm = false;
-        useSystemAsm = false;
-        useMasm = false;
-      };
-      yazi = (unstable.yazi.override {
-        inherit (_7zz);
-      }).overrideAttrs (old: {
-        buildInputs = (old.buildInputs or []) ++ [
-          final._7zz
-        ];
-      });
+      inherit (final) _7zz yazi;
     })
   ];
 }
