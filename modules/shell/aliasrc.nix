d0dbf18012394fc225a9f455c3285b7d01ac4fae
Jonas Hestdahl <hestdahl@gmail.com>
2024-11-03 21:37:32 +0100

    added stowdotfiles alias

--- modules/shell/aliasrc.nix
+++ modules/shell/aliasrc.nix
@@ -165,6 +165,8 @@
     recordings = "cd ~/recordings/ && ls";
     z="zathura";
     hl = "cd $GHREPOS/homelab/ && ls -A";
+    stowdotfiles='cd /home/jonash/repos/github/jonashestdahl/dotfiles-nix && stow -t /home/jonash *'
+
   };
   # Variables declared in profile.nix
 }
