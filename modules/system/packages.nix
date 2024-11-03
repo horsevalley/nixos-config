f878f8a542be4fa536cb3f400c37e418d07c5885
Jonas Hestdahl <hestdahl@gmail.com>
2024-11-03 22:19:13 +0100

    added zellij to packages

--- modules/system/packages.nix
+++ modules/system/packages.nix
@@ -27,6 +27,7 @@
     wev # Wayland Event Viewer
     wtype # xdotool type for wayland
     libinput # Handles input devices in Wayland compositors and provides a generic X.Org input driver
+    zellij # A terminal workspace with batteries included
 
     # Git
     git
