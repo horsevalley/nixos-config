21f495233af4ba0db69442b2bd198d564dfe19cd
Jonas Hestdahl <hestdahl@gmail.com>
2024-11-21 20:39:07 +0100

    kubernetes.nix: added useful comments and cloudflared

--- modules/virtualization/kubernetes.nix
+++ modules/virtualization/kubernetes.nix
@@ -4,12 +4,13 @@
 {
 
   environment.systemPackages = with pkgs; [
-    kubernetes
-    kubectl
-    kubectx
-    k3s
-    k9s
-    fluxcd
+    kubernetes # Production-Grade Container Orchestration, Scheduling and Management
+    kubectl # Kubernetes CLI
+    kubectx # Fast way to switch between clusters and namespaces in kubectl!
+    k3s # A lightweight Kubernetes distribution
+    k9s # Kubernetes CLI To Manage Your Clusters In Style
+    fluxcd # Open and extensible continuous delivery solution for Kubernetes
+    cloudflared # Cloudflare Tunnel daemon, Cloudflare Access toolkit, and DNS-over-HTTPS client
 
   ];
 
