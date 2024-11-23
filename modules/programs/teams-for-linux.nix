d051d8e71ed6a8adb08922d089c1c9f0cf6f3153
Jonas Hestdahl <hestdahl@gmail.com>
2024-11-23 21:03:26 +0100

    teams-for-linux: fixing so it can be run from rofi

--- modules/programs/teams-for-linux.nix
+++ modules/programs/teams-for-linux.nix
@@ -9,5 +9,17 @@
         exec ${pkgs.teams-for-linux}/bin/teams-for-linux "$@"
       '')
     ];
+
+    # Create desktop entry
+    environment.systemPackages = with pkgs; [
+      (makeDesktopItem {
+        name = "teams-for-linux";
+        desktopName = "Teams for Linux";
+        exec = "teams-for-linux";
+        icon = "${pkgs.teams-for-linux}/share/icons/hicolor/512x512/apps/teams-for-linux.png";
+        categories = [ "Network" "InstantMessaging" ];
+        terminal = false;
+      })
+    ];
   };
 }
